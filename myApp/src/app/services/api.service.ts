190a301cb9e08543902cda7f6b07876f2f7e2b24
7Kitsu7 <t1513300321@unitru.edu.pe>
2025-10-03 11:04:37 -0500

    Update api.service.ts

--- myApp/src/app/services/api.service.ts
+++ myApp/src/app/services/api.service.ts
@@ -19,8 +19,6 @@ export class ApiService {
   }
 
   get(endpoint: string) {
-    return this.http.get(`${this.baseUrl}/${endpoint}`, {
-      headers: this.getHeaders()
     return this.http.get(`${this.baseUrl}/${endpoint}`, { 
       headers: this.getHeaders() 
     });
